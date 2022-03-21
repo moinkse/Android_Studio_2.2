61c7d6352e867c697ab8388a10c07e3a89df00dc
moinkse <sstasik.sebastian@gmail.com>
2022-03-21 15:13:14 +0100

    initial commit

--- app/src/main/java/com/example/linear_layout_app/MainActivity.kt
+++ app/src/main/java/com/example/linear_layout_app/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.linear_layout_app
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
+import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -10,11 +11,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // get reference to ImageView
-        val iv_click_me = findViewById(R.id.star_image) as ImageView
+        val iv_click_me = findViewById(R.id.button) as Button
+
         // set on-click listener
         iv_click_me.setOnClickListener {
             // your code to perform when the user clicks on the ImageView
             Toast.makeText(this@MainActivity, "Hello there", Toast.LENGTH_SHORT).show()
         }
     }
+
 }
